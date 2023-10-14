255721872ed371083087f5c3f26554650738243d
subhan <suvhan13@gmail.com>
2023-10-14 22:37:40 +0530

    custome/hook

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+// I need to reuse logic with custome hook
+// we can use other project also
+// Rules of hook aply on custome hook
+// 1. if logic contain any hook then you can make custome hook
+// 2. if function start with use then you can make custome hook
+function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError('Your browser does not support geolocation');
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { isLoading, error, position, getPosition };
+}
+
+export default function App() {
+  const [countClicks, setCountClicks] = useState(0);
+  const {
+    isLoading,
+    error,
+    position: { lat, lng },
+    getPosition,
+  } = useGeolocation();
+
+  function handleCount() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={handleCount} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{' '}
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
